417ca5c57b96350c5b33ad238afe8ead8a4404c0
kenlo <kenlo.dinhvan@gmail.com>
2019-03-21 22:32:50 -0400

    Initial commit

---
+++ enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/MiscAcceptanceTest.scala
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j Enterprise Edition. The included source
+ * code can be redistributed and/or modified under the terms of the
+ * GNU AFFERO GENERAL PUBLIC LICENSE Version 3
+ * (http://www.fsf.org/licensing/licenses/agpl-3.0.html) with the
+ * Commons Clause, as found in the associated LICENSE.txt file.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * Neo4j object code can be licensed independently from the source
+ * under separate terms from the AGPL. Inquiries can be directed to:
+ * licensing@neo4j.com
+ *
+ * More information is also available at:
+ * https://neo4j.com/licensing/
+ */
+package org.neo4j.internal.cypher.acceptance
+
+import org.neo4j.cypher.ExecutionEngineFunSuite
+import org.neo4j.internal.cypher.acceptance.CypherComparisonSupport._
+
+class MiscAcceptanceTest extends ExecutionEngineFunSuite with CypherComparisonSupport {
+
+  // This test verifies a bugfix in slotted runtime
+  test("should be able to compare integers") {
+    val query = """
+      UNWIND range(0, 1) AS i
+      UNWIND range(0, 1) AS j
+      WITH i, j
+      WHERE i <> j
+      RETURN i, j"""
+
+    val result = executeWith(Configs.Interpreted + Configs.Morsel, query)
+    result.toList should equal(List(Map("j" -> 1, "i" -> 0), Map("j" -> 0, "i" -> 1)))
+  }
+
+  test("order by after projection") {
+    val query =
+      """
+        |UNWIND [ 1,2 ] as x
+        |UNWIND [ 3,4 ] as y
+        |RETURN x AS y, y as y3
+        |ORDER BY y
+      """.stripMargin
+
+    val result = executeWith(Configs.All, query, expectedDifferentResults = Configs.OldAndRule)
+    result.toList should equal(List(Map("y" -> 1, "y3" -> 3), Map("y" -> 1, "y3" -> 4), Map("y" -> 2, "y3" -> 3), Map("y" -> 2, "y3" -> 4)))
+  }
+
+  test("should unwind nodes") {
+    val n = createNode("prop" -> 42)
+
+    val query = "UNWIND $nodes AS n WITH n WHERE n.prop = 42 RETURN n"
+    val result = executeWith(Configs.All - Configs.Version2_3, query, params = Map("nodes" -> List(n)))
+
+    result.toList should equal(List(Map("n" -> n)))
+  }
+
+  test("should unwind nodes from literal list") {
+    val n = createNode("prop" -> 42)
+
+    val query = "UNWIND [$node] AS n WITH n WHERE n.prop = 42 RETURN n"
+    val result = executeWith(Configs.All - Configs.Version2_3, query, params = Map("node" -> n))
+
+    result.toList should equal(List(Map("n" -> n)))
+  }
+
+  test("should unwind relationships") {
+    val a = createNode()
+    val b = createNode()
+    val r = relate(a, b, "prop" -> 42)
+
+    val query = "UNWIND $relationships AS r WITH r WHERE r.prop = 42 RETURN r"
+    val result = executeWith(Configs.All - Configs.Version2_3, query, params = Map("relationships" -> List(r)))
+
+    result.toList should equal(List(Map("r" -> r)))
+  }
+
+  test("should unwind relationships from literal list") {
+    val a = createNode()
+    val b = createNode()
+    val r = relate(a, b, "prop" -> 42)
+
+    val query = "UNWIND [$relationship] AS r WITH r WHERE r.prop = 42 RETURN r"
+    val result = executeWith(Configs.All - Configs.Version2_3, query, params = Map("relationship" -> r))
+
+    result.toList should equal(List(Map("r" -> r)))
+  }
+
+  test("should be able to use long values for LIMIT in interpreted runtime") {
+    val a = createNode()
+    val b = createNode()
+
+    val limit: Long = Int.MaxValue + 1l
+    // If we would use Ints for storing the limit, then we would end up with "limit 0"
+    // thus, if we actually return the two nodes, then it proves that we used a long
+    val query = "MATCH (n) RETURN n LIMIT " + limit
+    val worksCorrectlyInConfig = Configs.Version3_4 + Configs.Version3_3 - Configs.AllRulePlanners
+    // the query will work in all configs, but only have the correct result in those specified configs
+    // Also: It Will work on 3.2 once 3.2.12 is out AND on 3.3 once 3.3.6 is out
+    val result = executeWith(Configs.All, query, Configs.All - worksCorrectlyInConfig)
+    result.toList should equal(List(Map("n" -> a), Map("n" -> b)))
+  }
+}
---
+++ tools/src/main/java/org/neo4j/tools/dump/InconsistencyReportReader.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j Enterprise Edition. The included source
+ * code can be redistributed and/or modified under the terms of the
+ * GNU AFFERO GENERAL PUBLIC LICENSE Version 3
+ * (http://www.fsf.org/licensing/licenses/agpl-3.0.html) with the
+ * Commons Clause, as found in the associated LICENSE.txt file.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * Neo4j object code can be licensed independently from the source
+ * under separate terms from the AGPL. Inquiries can be directed to:
+ * licensing@neo4j.com
+ *
+ * More information is also available at:
+ * https://neo4j.com/licensing/
+ */
+package org.neo4j.tools.dump;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.neo4j.tools.dump.inconsistency.Inconsistencies;
+
+/**
+ * Reads CC inconsistency reports. Example of entry:
+ *
+ * <pre>
+ * ERROR: The referenced relationship record is not in use.
+ *     Node[3496089,used=true,rel=14833798,prop=13305361,labels=Inline(0x1000000006:[6]),light,secondaryUnitId=-1]
+ *     Inconsistent with: Relationship[14833798,used=false,source=0,target=0,type=0,sPrev=0,sNext=0,tPrev=0,tNext=0,
+ *     prop=0,secondaryUnitId=-1,!sFirst,!tFirst]
+ * </pre>
+ */
+public class InconsistencyReportReader
+{
+    private final Inconsistencies inconsistencies;
+
+    public InconsistencyReportReader( Inconsistencies inconsistencies )
+    {
+        this.inconsistencies = inconsistencies;
+    }
+
+    public void read( File file ) throws IOException
+    {
+        try ( BufferedReader reader = new BufferedReader( new FileReader( file ) ) )
+        {
+            read( reader );
+        }
+    }
+
+    public void read( BufferedReader bufferedReader ) throws IOException
+    {
+        int state = 0; // 0:inconsistency description, 1:entity, 2:inconsistent with
+        String line;
+        while ( (line = bufferedReader.readLine()) != null )
+        {
+            line = line.trim();
+            if ( state == 0 )
+            {
+                if ( !line.contains( "ERROR" ) && !line.contains( " WARNING " ) )
+                {
+                    continue;
+                }
+            }
+            else if ( state == 1 )
+            {
+                tryPropagate( line );
+            }
+            else if ( state == 2 )
+            {
+                if ( line.startsWith( "Inconsistent with: " ) )
+                {
+                    line = line.substring( "Inconsistent with: ".length() );
+                }
+
+                tryPropagate( line );
+            }
+            state = (state + 1) % 3;
+        }
+    }
+
+    private void tryPropagate( String line )
+    {
+        String entityType = entityType( line );
+        long id = id( line );
+        if ( entityType != null && id != -1 )
+        {
+            propagate( entityType, id );
+        }
+    }
+
+    private void propagate( String entityType, long id )
+    {
+        switch ( entityType )
+        {
+        case "Relationship":
+            inconsistencies.relationship( id );
+            break;
+        case "Node":
+            inconsistencies.node( id );
+            break;
+        case "Property":
+            inconsistencies.property( id );
+            break;
+        case "RelationshipGroup":
+            inconsistencies.relationshipGroup( id );
+            break;
+        case "IndexRule":
+            inconsistencies.schemaIndex( id );
+            break;
+        case "IndexEntry":
+            inconsistencies.node( id );
+            break;
+        default:
+            // it's OK, we just haven't implemented support for this yet
+        }
+    }
+
+    private long id( String line )
+    {
+        int bracket = line.indexOf( '[' );
+        if ( bracket > -1 )
+        {
+            int separator = min( getSeparatorIndex( ',', line, bracket ),
+                    getSeparatorIndex( ';', line, bracket ),
+                    getSeparatorIndex( ']', line, bracket ) );
+            int equally = line.indexOf( '=', bracket );
+            int startPosition = (isNotPlainId( bracket, separator, equally ) ? equally : bracket) + 1;
+            if ( separator > -1 )
+            {
+                return Long.parseLong( line.substring( startPosition, separator ) );
+            }
+        }
+        return -1;
+    }
+
+    private static int min( int... values )
+    {
+        int min = Integer.MAX_VALUE;
+        for ( int value : values )
+        {
+            min = Math.min( min, value );
+        }
+        return min;
+    }
+
+    private int getSeparatorIndex( char character, String line, int bracket )
+    {
+        int index = line.indexOf( character, bracket );
+        return index >= 0 ? index : Integer.MAX_VALUE;
+    }
+
+    private boolean isNotPlainId( int bracket, int comma, int equally )
+    {
+        return (equally > bracket) && (equally < comma);
+    }
+
+    private String entityType( String line )
+    {
+        int bracket = line.indexOf( '[' );
+        return bracket == -1 ? null : line.substring( 0, bracket );
+    }
+}
